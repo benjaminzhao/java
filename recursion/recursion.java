b50976015eae7f2075a5f632813137d548e4846e
benjaminzhao <benjamin.zhao.1983@gmail.com>
2013-09-24 16:09:40 -0400

    recursion

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -18,7 +18,7 @@ public class recursion
 		double[] start_array = producer(length);
 		now = new Date();
 		System.out.println( dft.format(now) + " array produced " + length);
-		System.out.println( Arrays.toString(start_array) );
+		//System.out.println( Arrays.toString(start_array) );
 		
 		double[] end_array1 = sort(start_array);
 		now = new Date();
@@ -99,13 +99,13 @@ public class recursion
 		a_L = Arrays.copyOfRange(a, 0, j+1);//0~j
 		double[] a_L_new = new double[a_L.length];
 		a_L_new = Qsort(a_L);		
-		System.out.println( Arrays.toString(a_L) );
+		//System.out.println( Arrays.toString(a_L) );
 		
 		double[] a_R = new double[len-j-1];
 		a_R = Arrays.copyOfRange(a, j+1, len);//j+1~len-1
 		double[] a_R_new = new double[a_R.length];
 		a_R_new = Qsort(a_R);
-		System.out.println( Arrays.toString(a_R) );
+		//System.out.println( Arrays.toString(a_R) );
 		
 		double[] a_LR = new double[a_L_new.length+a_R_new.length];
 		System.arraycopy(a_L_new,0, a_LR, 0, a_L_new.length);
