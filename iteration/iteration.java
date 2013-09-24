7b6e488127b9a39602530ede688b463275e56b34
benjaminzhao <benjamin.zhao.1983@gmail.com>
2013-09-24 15:12:48 -0400

    iteration

--- iteration/iteration.java
+++ iteration/iteration.java
@@ -7,18 +7,17 @@ import java.io.*;
 
 public class iteration
 {
-	//public static int i = 0;
-	
+	public static int cnt = 0;
 	public static void main(String args[]) throws IOException
 	{
 		SimpleDateFormat dft = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
 		Date now = new Date();
 		System.out.println( dft.format(now) + " start");
 		
-		int length = ;
+		int length = Integer.parseInt(args[0]);
 		double[] start_array = producer(length);
 		now = new Date();
-		System.out.println( dft.format(now) + " produced " + length);
+		System.out.println( dft.format(now) + " array produced " + length);
 		//System.out.println( Arrays.toString(start_array) );
 		
 		double[] end_array1 = sort(start_array);
@@ -26,13 +25,22 @@ public class iteration
 		System.out.println( dft.format(now) + " sorted by sort()");
 		//System.out.println( Arrays.toString(end_array1) );
 		
-		
-		double[] end_array2 = Qsort(start_array);
+		try
+		{
+			double[] end_array2 = Qsort(start_array);
+			System.out.println( "Qsort "+ cnt);
+		}
+		catch(Throwable e)
+		{
+			System.out.println( "Qsort "+ cnt);
+			System.out.println( "catch exception: "+ e.getMessage() );
+			return; //throw e;
+		}
 		now = new Date();
 		System.out.println( dft.format(now) + " sorted by Qsort()");
 		//System.out.println( Arrays.toString(end_array2) );
 		
-		demo(0);
+		demo(length);
 		now = new Date();
 		System.out.println( dft.format(now) + " done");
 	}
@@ -66,11 +74,11 @@ public class iteration
 	public static double[] Qsort(double[] array)
 	{
 		double temp = 0;
-		
 		double[] a = array;
 		int len = a.length;
+		cnt++;
 		//System.out.println( Arrays.toString(a) );
-		if(a.length <= 1)
+		if(len <= 1)
 		{
 			return a;
 		}
@@ -88,30 +96,32 @@ public class iteration
 		a[0] = temp;
 
 		double[] a_L = new double[j+1];
-		double[] a_R = new double[len-j-1];
+		a_L = Arrays.copyOfRange(a, 0, j+1);//0~j
+		double[] a_L_new = new double[a_L.length];
+		a_L_new = Qsort(a_L);		
 		
-		a_L = Arrays.copyOfRange(a, 0, j+1);
-		a_R = Arrays.copyOfRange(a, j+1, len);
+		double[] a_R = new double[len-j-1];
+		a_R = Arrays.copyOfRange(a, j+1, len);//j+1~len-1
+		double[] a_R_new = new double[a_R.length];
+		a_R_new = Qsort(a_R);		
 		//System.out.println( Arrays.toString(a_L) );
 		//System.out.println( Arrays.toString(a_R) );
 		
-		double[] a_L_new = new double[a_L.length];
-		a_L_new = Qsort(a_L);
-		double[] a_R_new = new double[a_R.length];
-		a_R_new = Qsort(a_R);
-		
 		double[] a_LR = new double[a_L_new.length+a_R_new.length];
-		System.arraycopy(a_L_new,0, a_LR,0, a_L_new.length);
-		System.arraycopy(a_R_new,0, a_LR,a_L_new.length, a_R_new.length);
+		System.arraycopy(a_L_new,0, a_LR, 0, a_L_new.length);
+		System.arraycopy(a_R_new,0, a_LR, a_L_new.length, a_R_new.length);
 		return a_LR;
 	}
 	
 	//simple demo function
 	public static void demo(int i)
 	{
-		System.out.println("this is step: "+i);
-		i++;
-		if(i < 10)
+		//System.out.println("this is step: "+i);
+		i--;
+		if(i > 0)
 			demo(i);
+		else
+			System.out.println("this is step: " + i);
 	}
+	
 }
